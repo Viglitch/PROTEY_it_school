f2b6958cf1cff3d30b63f4b9b6ba297a461abf72
Viglitch <varvarafeofanova49@gmail.com>
2025-07-14 12:21:22 +0300

    server_main fix

--- PROTEY_it_school/HW6/server_main.cpp
+++ PROTEY_it_school/HW6/server_main.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <arpa/inet.h>
+
+constexpr int MAX_EVENTS = 10;
+
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
@@ -13,8 +25,89 @@ int main(int argc, char* argv[]) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    epoll_fd = epoll_create1(0);
+    if (epoll_fd == -1) {
+        perror("epoll_create1");
+        exit(EXIT_FAILURE);
+    }
+
+    ev.events = EPOLLIN;
+    ev.data.fd = server_fd;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev) == -1) {
+        perror("epoll_ctl: server_fd");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Server listening on port " << port << std::endl;
+
     while (true) {
         int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        if (nfds == -1) {
+            perror("epoll_wait");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < nfds; ++i) {
+            if (events[i].data.fd == server_fd) {
+                struct sockaddr_in client_addr;
+                socklen_t client_len = sizeof(client_addr);
+                int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+                if (client_fd == -1) {
+                    perror("accept");
+                    continue;
+                }
+
+                char client_ip[INET_ADDRSTRLEN];
+                inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+                std::cout << "New connection from " << client_ip << ":" << ntohs(client_addr.sin_port) << std::endl;
+
+                ev.events = EPOLLIN | EPOLLET;
+                ev.data.fd = client_fd;
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev) == -1) {
+                    perror("epoll_ctl: client_fd");
+                    close(client_fd);
+                }
+            }
+            else {
+                int client_fd = events[i].data.fd;
+                char buffer[1024];
+                int bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
+
+                if (bytes_read <= 0) {
+                    if (bytes_read == 0) {
+                        std::cout << "Client disconnected" << std::endl;
+                    }
+                    else {
+                        perror("recv");
+                    }
+                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
+                    close(client_fd);
+                }
+                else {
+                }
+            }
+        }
     }
 
     close(server_fd);
