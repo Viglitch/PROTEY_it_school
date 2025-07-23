feca9cc0db68b726f572a1b4ffd4b1c2a142fd1d
Viglitch <varvarafeofanova49@gmail.com>
2025-07-23 16:14:06 +0300

    http api

--- PROTEY_it_school/PGW/pgw_server.cpp
+++ PROTEY_it_school/PGW/pgw_server.cpp
@@ -12,94 +12,78 @@
 #include <mutex>
 #include <chrono>
 #include <nlohmann/json.hpp>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sstream>
 
 using json = nlohmann::json;
 
 struct Config {
-    int port;
+    int udp_port;
+    int http_port;
     int session_timeout_sec;
-    int max_offload_sessions;
     std::unordered_set<std::string> blacklist;
     std::string cdr_path;
-    int max_log_size_mb;
-};
+} config;
 
-struct Session {
-    std::string session_id;
-    time_t created_at;
-    std::string client_ip;
-    uint16_t client_port;
-};
-
-
-Config config;
 std::unordered_map<std::string, Session> active_sessions;
 std::mutex sessions_mutex;
+bool server_running = true;
+
+void start_http_server() {
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(config.http_port);
+
+    bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
+    listen(server_fd, 10);
+
+    std::cout << "HTTP API started on port " << config.http_port << std::endl;
+
+    while (server_running) {
+        sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
+        int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+
+        char buffer[4096] = { 0 };
+        read(client_fd, buffer, 4096);
+
+        std::string request(buffer);
+        std::string response;
 
 
-void load_config(const std::string& path) {
-    std::ifstream config_file(path);
-    if (!config_file) {
-        throw std::runtime_error("Cannot open config file");
-    }
+        if (request.find("GET /check_subscriber?imsi=") != std::string::npos) {
+            size_t start = request.find("imsi=") + 5;
+            size_t end = request.find(" ", start);
+            std::string imsi = request.substr(start, end - start);
 
-    json j;
-    config_file >> j;
+            std::lock_guard<std::mutex> lock(sessions_mutex);
+            auto it = active_sessions.find(imsi);
 
-    config.port = j["server"]["port"];
-    config.session_timeout_sec = j["server"]["session_timeout_sec"];
-    config.max_offload_sessions = j["server"]["max_offload_sessions"];
-    config.cdr_path = j["logging"]["cdr_path"];
-    config.max_log_size_mb = j["logging"]["max_file_size_mb"];
-
-    for (const auto& imsi : j["blacklist"]) {
-        config.blacklist.insert(imsi.get<std::string>());
-    }
-}
-
-
-void log_cdr(const std::string& imsi, const std::string& action) {
-    std::ofstream cdr_file(config.cdr_path, std::ios::app);
-    if (!cdr_file) {
-        std::cerr << "Cannot open CDR log file" << std::endl;
-        return;
-    }
-
-    time_t now = time(nullptr);
-    char timestamp[20];
-    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", localtime(&now));
-
-    cdr_file << timestamp << " | IMSI: " << imsi << " | Action: " << action << "\n";
-}
-
-
-void cleanup_sessions() {
-    while (true) {
-        std::this_thread::sleep_for(std::chrono::seconds(60));
-
-        std::lock_guard<std::mutex> lock(sessions_mutex);
-        time_t now = time(nullptr);
-
-        for (auto it = active_sessions.begin(); it != active_sessions.end(); ) {
-            if (now - it->second.created_at >= config.session_timeout_sec) {
-                log_cdr(it->first, "expired (session ID: " + it->second.session_id + ")");
-                it = active_sessions.erase(it);
+            json j;
+            if (it != active_sessions.end()) {
+                j["status"] = "active";
+                j["session_id"] = it->second.session_id;
+                j["created_at"] = it->second.created_at;
             }
             else {
-                ++it;
+                j["status"] = "inactive";
             }
+            response = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n" + j.dump();
         }
+
+        else if (request.find("GET /stop") != std::string::npos) {
+            server_running = false;
+            response = "HTTP/1.1 200 OK\r\n\r\nServer stopping...";
+        }
+        else {
+            response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        }
+
+        write(client_fd, response.c_str(), response.size());
+        close(client_fd);
     }
-}
-
-std::string generate_session_id() {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_int_distribution<> dis(100000, 999999);
-    return "SID-" + std::to_string(dis(gen));
-}
-
-
-void handle_client(int sockfd, sockaddr_in client_addr, char* buffer, int bytes_received) {
-
+    close(server_fd);
 }
